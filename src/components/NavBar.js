d268409650a6c785523d43ec031557905611a397
Leon simintei <mallfs@icloud.com>
2023-01-14 12:37:00 +0300

    assignment complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,11 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   const myLinks = links.map((link)=>{
-    return <a key={{link}} href={{link}}>{link}</a>
+    return <a href={`#`+link} key={link} >{link}</a>
   })
-  return <nav>{myLinks}</nav>;
+  return <nav>
+    {myLinks}
+    </nav>;
 }
 
 export default NavBar;
